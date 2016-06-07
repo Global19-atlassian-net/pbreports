5cab9b5ddd3d550462a12310fe8f28cf35b6d0d6
Ben Lerch <blerch@smtp.nanofluidics.com>
2016-06-07 11:36:42 -0700

    updated report_spec.py and spec jsons

--- pbreports/report/report_spec.py
+++ pbreports/report/report_spec.py
@@ -19,8 +19,7 @@ class MetaAttribute(object):
                              d['description'], d["type"])
 
     def as_attribute(self, value):
-        assert type(value).__name__ == self.type, "{v} != {t}".format(
-            v=value, t=self.type)
+        assert type(value).__name__ == self.type, "{v} != {t}".format(v=value, t=self.type)
         return Attribute(self.id, value=value, name=self.name)
 
 
@@ -37,8 +36,10 @@ class MetaColumn(object):
         return MetaColumn(d['id'].split(".")[-1], d['header'],
                           d['description'], d["type"])
 
-    def as_column(self, header=None, values=()):
-        return Column(self.id, header=header, values=values)
+    def as_column(self, values=()):
+	for value in values:
+		assert type(value).__name__ == self.type, "{v} != {t}".format(v=value, t=self.type)
+        return Column(self.id, header=self.header, values=values)
 
 
 class MetaTable(object):
@@ -58,15 +59,14 @@ class MetaTable(object):
     def get_meta_column(self, id_):
         return self._col_dict[id_]
 
-    def as_table(self, title=None, columns=()):
-        return Table(self.id, title=title, columns=columns)
+    def as_table(self, columns=()):
+        return Table(self.id, title=self.title, columns=columns)
 
 
 class MetaPlot(object):
 
-    def __init__(self, id_, title, description, caption, xlab, ylab):
+    def __init__(self, id_, description, caption, xlab, ylab):
         self.id = id_
-        self.title = title
         self.description = description
         self.caption = caption
         self.xlab = xlab
@@ -74,10 +74,10 @@ class MetaPlot(object):
 
     @staticmethod
     def from_dict(d):
-        return MetaPlot(d['id'].split(".")[-1], d['title'], d['description'], d['caption'], d['xlab'], d['ylab'])
+        return MetaPlot(d['id'].split(".")[-1], d['description'], d['caption'], d['xlab'], d['ylab'])
 
-    def as_plot(self, image, caption=None, thumbnail=None):
-        return Plot(self.id, image=image, caption=caption, thumbnail=thumbnail)
+    def as_plot(self, image, thumbnail):
+        return Plot(self.id, image=image, caption=self.caption, thumbnail=thumbnail)
 
 
 class MetaPlotGroup(object):
@@ -98,8 +98,8 @@ class MetaPlotGroup(object):
     def get_meta_plot(self, id_):
         return self._plot_dict[id_]
 
-    def as_plotgroup(self, title, legend, plots):
-        return PlotGroup(self.id, title=title, legend=legend, plots=plots)
+    def as_plotgroup(self, thumbnail, plots=()):
+        return PlotGroup(self.id, title=self.title, legend=self.legend, thumbnail=thumbnail, plots=plots)
 
 
 class MetaReport(object):
@@ -119,10 +119,8 @@ class MetaReport(object):
         d = json.load(open(json_str))
         json.dumps(d)
         return MetaReport(d['id'], d['title'],
-                          [MetaAttribute.from_dict(a)
-                           for a in d['attributes']],
-                          [MetaPlotGroup.from_dict(p)
-                           for p in d['plotgroups']],
+                          [MetaAttribute.from_dict(a) for a in d['attributes']],
+                          [MetaPlotGroup.from_dict(p) for p in d['plotgroups']],
                           [MetaTable.from_dict(t) for t in d['tables']])
 
     def get_meta_attribute(self, id_):
