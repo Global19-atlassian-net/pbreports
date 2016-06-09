57fd669ff59eab4bf07d4905cddefc4f0dbc7df2
Ben Lerch <blerch@smtp.nanofluidics.com>
2016-06-09 14:59:59 -0700

    attempt to fix W function, this may not work

--- pbreports/report/mapping_stats.py
+++ pbreports/report/mapping_stats.py
@@ -978,12 +978,12 @@ def summarize_report(report_file, out=sys.stdout):
     report = load_report_from_json(report_file)
     attr = {a.id: a.value for a in report.attributes}
     W("%s:" % report_file)
-    W(meta_rpt.get_meta_attribute(Constants.A_SUBREAD_CONCORDANCE).name.upper() +  ": {a}".format(a=attr[Constants.A_SUBREAD_CONCORDANCE]))
-    W(meta_rpt.get_meta_attribute(Constants.A_NSUBREADS).name.upper() + ": {a}".format(a=attr[Constants.A_NSUBREADS]))
-    W(meta_rpt.get_meta_attribute(Constants.A_NREADS).name.upper() + ": {a}".format(a=attr[Constants.A_NREADS]))
-    W(meta_rpt.get_meta_attribute(Constants.A_SUBREAD_NBASES).name.upper() + ": {a}".format(a=attr[Constants.A_SUBREAD_NBASES]))
-    W(meta_rpt.get_meta_attribute(Constants.A_READLENGTH).name.upper() + ": {a}".format(a=attr[Constants.A_READLENGTH]))
-    W(meta_rpt.get_meta_attribute(Constants.A_SUBREAD_LENGTH).name.upper() + ": {a}".format(a=attr[Constants.A_SUBREAD_LENGTH]))
+    W("  {n}: {a}".format(n=meta_rpt.get_meta_attribute(Constants.A_SUBREAD_CONCORDANCE).name.upper(), a=attr[Constants.A_SUBREAD_CONCORDANCE]))
+    W("  {n}: {a}".format(n=meta_rpt.get_meta_attribute(Constants.A_NSUBREADS).name.upper(), a=attr[Constants.A_NSUBREADS]))
+    W("  {n}: {a}".format(n=meta_rpt.get_meta_attribute(Constants.A_NREADS).name.upper(), a=attr[Constants.A_NREADS]))
+    W("  {n}: {a}".format(n=meta_rpt.get_meta_attribute(Constants.A_SUBREAD_NBASES).name.upper(), a=attr[Constants.A_SUBREAD_NBASES]))
+    W("  {n}: {a}".format(n=meta_rpt.get_meta_attribute(Constants.A_READLENGTH).name.upper(), a=attr[Constants.A_READLENGTH]))
+    W("  {n}: {a}".format(n=meta_rpt.get_meta_attribute(Constants.A_SUBREAD_LENGTH).name.upper(), a=attr[Constants.A_SUBREAD_LENGTH]))
 
 def run_and_write_report(alignment_file, json_report, report_func=to_report):
     output_dir = os.path.dirname(json_report)
