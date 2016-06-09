b82f788da32af699149c9826ae5f081d2bbe16d8
Ben Lerch <blerch@smtp.nanofluidics.com>
2016-06-08 17:03:08 -0700

    update setup.py to recognize specs dir

--- setup.py
+++ setup.py
@@ -25,6 +25,7 @@ setup(
     author_email='pbiDevNet@pacificbiosciences.com',
     license='LICENSE.txt',
     packages=find_packages(),
+    package_data={'pbreports': ['reports/specs/*.json']}
     zip_safe=False,
     install_requires=_get_requirements(_get_local_file(_REQUIREMENTS_FILE)),
     entry_points={'console_scripts': [
