286099f571445c2e92ab5085d4bd348379e9707c
Ben Lerch <blerch@smtp.nanofluidics.com>
2016-08-08 00:38:49 -0700

    remove debugging line

--- tests/unit/test_pbreports_report_isoseq.py
+++ tests/unit/test_pbreports_report_isoseq.py
@@ -58,7 +58,6 @@ class _TestIsoSeqBase(unittest.TestCase):
     def test_validate_report(self):
         r = self._to_report()
         self.assertIsInstance(r, Report)
-        print pformat(r.to_dict())
         validate_report_complete(self, r)
 
     def test_report_id(self):
