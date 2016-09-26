91271a29440a5fcde78263e50a5ac4ae36717f3c
natechols <nathaniel.echols@gmail.com>
2016-09-26 08:25:40 -0700

    simplify amplicon analysis input report to use LAA-provided CSV

--- tests/unit/test_pbreports_report_amplicon_analysis_input.py
+++ tests/unit/test_pbreports_report_amplicon_analysis_input.py
@@ -7,10 +7,7 @@ from pprint import pformat
 
 from pbcommand.models.report import Report
 
-from pbreports.report.amplicon_analysis_input import (run_to_report,
-                                                      parse_summary,
-                                                      parse_mappings,
-                                                      tabulate_results)
+from pbreports.report.amplicon_analysis_input import run_to_report
 from base_test_case import LOCAL_DATA, run_backticks, validate_report_complete
 
 log = logging.getLogger(__name__)
@@ -23,26 +20,12 @@ class TestLongAmpliconAnalysisPcrReport(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
-        summary_name = 'amplicon_analysis_summary.csv'
-        cls.summary_file = os.path.join(DATA_DIR, summary_name)
-        zmw_name = 'amplicon_analysis_zmws.json'
-        cls.zmw_file = os.path.join(DATA_DIR, zmw_name)
-        log.info("Using {s} and {z}".format(
-            s=cls.summary_file, z=cls.zmw_file))
-
-    def test_process_files(self):
-        s = parse_summary(self.summary_file)
-        c = parse_mappings(self.zmw_file)
-        d = tabulate_results(s, c)
-        for bc_id, fields in d.iteritems():
-            total = 0
-            for k, v in fields.iteritems():
-                if k.endswith("_pct"):
-                    total += v
-            self.assertAlmostEqual(total, 1.0, places=3)
+        summary_name = 'amplicon_analysis_input.csv'
+        cls.csv_file = os.path.join(DATA_DIR, summary_name)
+        log.info("Using {s}".format(s=cls.csv_file))
 
     def test_make_report(self):
-        report = run_to_report(self.summary_file, self.zmw_file)
+        report = run_to_report(self.csv_file)
         validate_report_complete(self, report)
         self.assertTrue(isinstance(report, Report))
         log.info(pformat(report.to_dict()))
@@ -59,10 +42,9 @@ class TestLongAmpliconAnalysisPcrReport(unittest.TestCase):
         t.close()
         report_json = t.name
         exe = "amplicon_analysis_input_report"
-        cmd = "{e} --debug {s} {z} {r}".format(e=exe,
-                                               s=self.summary_file,
-                                               z=self.zmw_file,
-                                               r=report_json)
+        cmd = "{e} --debug {s} {r}".format(e=exe,
+                                           s=self.csv_file,
+                                           r=report_json)
         exit_code = run_backticks(cmd)
         self.assertEqual(exit_code, 0)
         with open(report_json, 'r') as f:
