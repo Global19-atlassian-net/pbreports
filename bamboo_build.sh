b064e94b0d8963a7877df80fc660bc8e6eb16dfb
natechols <nathaniel.echols@gmail.com>
2017-03-07 11:14:59 -0800

    use central libhdf5

--- bamboo_build.sh
+++ bamboo_build.sh
@@ -1,13 +1,15 @@
 #!/bin/bash -ex
 
+source /mnt/software/Modules/current/init/bash
+module load hdf5-tools/1.8.16
+module load python/2.7.9
+
 mkdir -p tmp
-/opt/python-2.7.9/bin/python /mnt/software/v/virtualenv/13.0.1/virtualenv.py tmp/venv
+python /mnt/software/v/virtualenv/13.0.1/virtualenv.py tmp/venv
 source tmp/venv/bin/activate
 
 (cd repos/PacBioTestData && make python)
 (cd repos/pbcommand && make install)
-(cd .circleci && bash installHDF5.sh)
-export HDF5_DIR=$PWD/.circleci/prefix
 pip install -r PB_REQUIREMENTS.txt
 (cd repos/pbcore && make install)
 pip install -r REQUIREMENTS.txt
