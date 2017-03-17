533aedc19c41e2f708951c1d2c1de0016915c41f
Ben Lerch <blerch@smtp.nanofluidics.com>
2017-03-17 16:06:18 -0700

    add internal-data-present decorator to unit test of control report

--- tests/unit/test_pbreports_report_control.py
+++ tests/unit/test_pbreports_report_control.py
@@ -16,6 +16,11 @@ from pbreports.report.control import to_report
 log = logging.getLogger(__name__)
 
 
+def _internal_data():
+    if os.path.exists("/pbi/dept/secondary/siv/testdata"):
+        return True
+    return False
+
 def get_control_subreadset():
     ss = ('/pbi/dept/secondary/siv/testdata/SA3-Sequel/lambda/'
            '314/3140099/r54099_20160830_222805/3_C01_control/'
@@ -33,6 +38,8 @@ class TestControlRpt(unittest.TestCase):
         if op.exists(self._output_dir):
             shutil.rmtree(self._output_dir)
 
+    @unittest.skipIf(not _internal_data(),
+                     "Internal data not available")
     def test_make_control_report(self):
               
         ss = get_control_subreadset()
