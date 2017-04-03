7576fe52c0596d1fba699f08a372346f2452d5a0
natechols <nathaniel.echols@gmail.com>
2017-04-03 11:52:06 -0700

    oops

--- pbreports/io/validators.py
+++ pbreports/io/validators.py
@@ -4,7 +4,7 @@ import functools
 
 from pbcore.util.Process import backticks
 from pbcommand.utils import nfs_exists_check
-from pbcommand.validators import te_output_dir, validate_fofn, fofn_to_files, validate_report
+from pbcommand.validators import validate_output_dir, validate_fofn, fofn_to_files, validate_report
 
 log = logging.getLogger(__name__)
 
