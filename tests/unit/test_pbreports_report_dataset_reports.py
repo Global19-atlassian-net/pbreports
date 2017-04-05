f12e4e515d8ebab2d1fabdc5a6569862c7888392
natechols <nathaniel.echols@gmail.com>
2017-04-05 10:29:40 -0700

    check datastore file IDs

--- tests/unit/test_pbreports_report_dataset_reports.py
+++ tests/unit/test_pbreports_report_dataset_reports.py
@@ -27,7 +27,11 @@ class TestDataSetReports(unittest.TestCase):
     def test_sequel_subreads(self):
         data = pbtestdata.get_file("subreads-sequel")
         datastore = dataset_reports.to_reports(data, self._output_dir)
-        self.assertEqual(len(datastore.files), 3)
+        datastore_files = [f for u,f in datastore.files.iteritems()]
+        self.assertEqual(sorted([f.file_id for f in datastore_files]),
+                         ["pbreports.tasks.adapter_report_xml",
+                          "pbreports.tasks.filter_stats_report_xml",
+                          "pbreports.tasks.loading_report_xml"])
 
     def test_integration(self):
         ds_out = op.join(self._output_dir, "datastore.json")
@@ -37,11 +41,20 @@ class TestDataSetReports(unittest.TestCase):
         self.assertEqual(c, 0)
         self.assertTrue(op.exists(ds_out))
         datastore = DataStore.load_from_json(ds_out)
-        self.assertEqual(len(datastore.files), 3)
+        datastore_files = [f for u,f in datastore.files.iteritems()]
+        self.assertEqual(sorted([f.file_id for f in datastore_files]),
+                         ["pbreports.tasks.adapter_report_xml",
+                          "pbreports.tasks.filter_stats_report_xml",
+                          "pbreports.tasks.loading_report_xml"])
 
     @unittest.skipUnless(op.exists("/pbi/dept/secondary/siv"),
                          "Requires local test data")
     def test_with_control(self):
         data = get_control_subreadset()
         datastore = dataset_reports.to_reports(data, self._output_dir)
-        self.assertEqual(len(datastore.files), 4)
+        datastore_files = [f for u,f in datastore.files.iteritems()]
+        self.assertEqual(sorted([f.file_id for f in datastore_files]),
+                         ["pbreports.tasks.adapter_report_xml",
+                          "pbreports.tasks.control_report",
+                          "pbreports.tasks.filter_stats_report_xml",
+                          "pbreports.tasks.loading_report_xml"])
