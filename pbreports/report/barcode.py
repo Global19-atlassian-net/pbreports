cb40de10bac61225da3af9949f079ba21b77d41a
natechols <nathaniel.echols@gmail.com>
2017-05-22 17:36:52 -0700

    update barcode report to include number of subreads

--- pbreports/report/barcode.py
+++ pbreports/report/barcode.py
@@ -17,7 +17,7 @@ from pbcore.io import openDataSet, BarcodeSet
 from pbreports.io.specs import *
 
 log = logging.getLogger(__name__)
-__version__ = '0.6'
+__version__ = '0.7'
 
 spec = load_spec("barcode")
 
@@ -28,13 +28,14 @@ class Constants(object):
     DRIVER_EXE = "python -m pbreports.report.barcode --resolved-tool-contract"
     C_BARCODE = 'barcode'
     C_NREADS = 'number_of_reads'
+    C_NSUBREADS = 'number_of_subreads'
     C_NBASES = 'number_of_bases'
 
 
-def _labels_reads_iterator(reads, barcodes, subreads=True):
+def iter_reads_by_barcode(reads, barcodes):
     with openDataSet(reads) as ds:
         for er in ds.externalResources:
-            if er.barcodes != barcodes:
+            if er.barcodes is not None and er.barcodes != barcodes:
                 raise ValueError("Mismatch between external resource " +
                                  "barcodes and input BarcodeSet: " +
                                  "{a} != {b}".format(a=er.barcodes,
@@ -43,23 +44,26 @@ def _labels_reads_iterator(reads, barcodes, subreads=True):
         zmws_by_barcode = defaultdict(set)
         reads_by_zmw = defaultdict(list)
         for rr in ds.resourceReaders():
-            for i, (b, z, q) in enumerate(zip(rr.pbi.bcForward,
-                                              rr.pbi.holeNumber,
-                                              rr.pbi.qId)):
+            for i, (f, r, z, q) in enumerate(zip(rr.pbi.bcForward,
+                                                 rr.pbi.bcReverse,
+                                                 rr.pbi.holeNumber,
+                                                 rr.pbi.qId)):
                 movie = rr.readGroupInfo(q).MovieName
-                zmws_by_barcode[b].add((movie, z))
+                zmws_by_barcode[(f,r)].add((movie, z))
                 reads_by_zmw[(movie, z)].append((rr, i))
         with BarcodeSet(barcodes) as ds_bc:
+            bc_ids = sorted(zmws_by_barcode.keys())
             bcs = [bc for bc in ds_bc]
-            for i_bc, barcode in enumerate(bcs):
-                zmws = sorted(list(zmws_by_barcode[i_bc]))
+            for i_bc, (barcode_fw, barcode_rev) in enumerate(bc_ids):
+                barcode_id = "{f}--{r}".format(f=bcs[barcode_fw].id,
+                                               r=bcs[barcode_rev].id)
+                zmws = sorted(list(zmws_by_barcode[(barcode_fw,barcode_rev)]))
                 for (movie, zmw) in zmws:
+                    qlen = n_subreads = 0
                     for rr, i_read in reads_by_zmw[(movie, zmw)]:
-                        qlen = rr.pbi.qEnd[i_read] - rr.pbi.qStart[i_read]
-                        barcode_id = "{f}--{r}".format(
-                            f=bcs[rr.pbi.bcForward[i_read]].id,
-                            r=bcs[rr.pbi.bcReverse[i_read]].id)
-                        yield barcode_id, barcode, ["n"] * qlen
+                        qlen += rr.pbi.qEnd[i_read] - rr.pbi.qStart[i_read]
+                        n_subreads += 1
+                    yield barcode_id, (barcode_fw, barcode_rev), qlen, n_subreads
 
 
 def run_to_report(reads, barcodes, subreads=True, dataset_uuids=()):
@@ -73,19 +77,22 @@ def run_to_report(reads, barcodes, subreads=True, dataset_uuids=()):
             self.label = label
             self.bases = 0
             self.reads = 0
+            self.subreads = 0
 
     label2row = {}
 
-    for label, barcode, read in _labels_reads_iterator(reads, barcodes,
-                                                       subreads=subreads):
+    for label, bcs, nbases, nsubreads in iter_reads_by_barcode(reads, barcodes):
         if not label in label2row:
             label2row[label] = MyRow(label)
-        label2row[label].bases += len(read)
+        label2row[label].bases += nbases
         label2row[label].reads += 1
+        label2row[label].subreads += nsubreads
 
     columns = [Column(Constants.C_BARCODE),
                Column(Constants.C_NREADS),
                Column(Constants.C_NBASES)]
+    if subreads:
+        columns.insert(2, Column(Constants.C_NSUBREADS))
 
     table = Table('barcode_table', columns=columns)
     labels = sorted(label2row.keys())
@@ -93,6 +100,8 @@ def run_to_report(reads, barcodes, subreads=True, dataset_uuids=()):
         row = label2row[label]
         table.add_data_by_column_id(Constants.C_BARCODE, label)
         table.add_data_by_column_id(Constants.C_NREADS, row.reads)
+        if subreads:
+            table.add_data_by_column_id(Constants.C_NSUBREADS, row.subreads)
         table.add_data_by_column_id(Constants.C_NBASES, row.bases)
 
     report = Report(spec.id, tables=[table],
